fe38b29dd2051560b4fdc8d77b91dd437dd86a47
Martin Hildebrandt <der.antikeks@googlemail.com>
2014-05-22 16:49:58 +0200

    working spheretree, may need optimization

--- game/spheretree.go
+++ game/spheretree.go
@@ -1,16 +1,17 @@
 package game
 
 import (
-	"log"
+	"fmt" // TODO: for debugging
 	m "math"
+	"strings"
 
 	"github.com/der-antikeks/gisp/math"
 )
 
 type SphereTree struct {
-	root      *Node
-	restraint float64
-	pool      []*Node
+	root                   *Node
+	restraint              float64
+	pool, sinsert, srecalc []*Node
 }
 
 func NewSphereTree(restraint float64) *SphereTree {
@@ -19,12 +20,25 @@ func NewSphereTree(restraint float64) *SphereTree {
 	}
 }
 
+// TODO: for debugging
+func (t *SphereTree) String() string {
+	s := fmt.Sprintf("SphereTree(%v), %v, %v, %v\n", t.restraint, len(t.pool), len(t.sinsert), len(t.srecalc))
+	if t.root != nil {
+		s += t.root.StringWalk(0)
+	} else {
+		s += "nil"
+	}
+	return s
+}
+
+// add empty node to pool
 func (t *SphereTree) put(n *Node) {
 	n.parent, n.children = nil, nil
 	n.center, n.radius = math.Vector{}, 0.0
 	t.pool = append(t.pool, n)
 }
 
+// get free node from pool or creates new if empty
 func (t *SphereTree) get() *Node {
 	if l := len(t.pool) - 1; l >= 0 {
 		n := t.pool[l]
@@ -38,9 +52,10 @@ func (t *SphereTree) get() *Node {
 func (t *SphereTree) Add(p math.Vector, r float64) *Node {
 	if t.root == nil {
 		t.root = t.get()
-		t.root.typ = BranchNode
+		t.root.typ = LeafNode // BranchNode
 		t.root.center = p
 		t.root.radius = r + t.restraint
+		return t.root
 	}
 
 	n := t.get()
@@ -48,61 +63,99 @@ func (t *SphereTree) Add(p math.Vector, r float64) *Node {
 	n.center = p
 	n.radius = r
 
-	t.insert(t.root, n)
+	// TODO: prevent Update()->parent before scheduleInsert
+	//t.scheduleInsert(n)
+	t.insert(n)
 	return n
 }
 
+func (t *SphereTree) scheduleInsert(n *Node) {
+	t.sinsert = append(t.sinsert, n)
+}
+
+func (t *SphereTree) scheduleRecalc(n *Node) {
+	t.srecalc = append(t.srecalc, n)
+}
+
+func (t *SphereTree) Update() {
+	// recalc
+	var n *Node
+	for len(t.srecalc) > 0 {
+		n, t.srecalc = t.srecalc[0], t.srecalc[1:]
+		t.recalc(n)
+	}
+
+	// insert
+	for len(t.sinsert) > 0 {
+		n, t.sinsert = t.sinsert[0], t.sinsert[1:]
+		t.insert(n)
+	}
+}
+
 // add node to root/traverse children
-func (t *SphereTree) insert(p, c *Node) {
-	if d := p.center.Sub(c.center).Length(); d+p.radius < c.radius {
-		// parent is inside of child
-		if p.parent == nil {
-			// parent is root
-			t.root = t.mergeNodes(p, c)
-			t.root.addChild(p)
-			p.parent = t.root
-
-			p = t.root
-		} else {
-			// this should not happen
-			log.Fatalln("parent is inside of new node but is not root!")
-		}
-	}
-
+func (t *SphereTree) insert(n *Node) {
 	var sibling *Node
-	var dist float64
 	mindist := m.Inf(1)
-	for _, s := range p.children {
-		dist = s.center.Sub(c.center).Length()
-		if dist < mindist {
-			if c.radius+dist < s.radius {
-				// close enough sibling
-				sibling = s
-				mindist = dist
-			}
+	t.root.walk(func(p *Node) bool {
+		dist := p.center.Sub(n.center).Length()
+		if p.radius+n.radius < dist {
+			// Disjoint
+			return false
 		}
+		if p.radius-n.radius < dist {
+			// Intersects
+			if p.typ == BranchNode {
+				return false
+			}
+		} else {
+			// Contains
+		}
+
+		// TODO: still have to think about whether that is optimal
+		//if dist+n.radius <= mindist {
+		if dist <= mindist {
+			mindist = dist
+			sibling = p
+		}
+
+		return true
+	})
+
+	if sibling == nil {
+		// child node is bigger than root
+		// create a new, bigger root
+		branch := t.mergeNodes(t.root, n)
+		branch.radius += t.restraint
+
+		branch.addChild(t.root)
+		t.root.parent = branch
+
+		t.root = branch
+		sibling = branch
 	}
+	if sibling.typ == LeafNode {
+		// nearest sibling is leaf, create branch wich encloses both
 
-	if sibling != nil {
-		// create new branch with sibling
-		branch := t.mergeNodes(sibling, c)
+		branch := t.mergeNodes(sibling, n)
+		branch.radius += t.restraint
+		branch.parent = sibling.parent
 
-		p.addChild(branch)
-		branch.parent = p
+		if sibling.parent == nil {
+			// sibling was root
+			t.root = branch
+		} else {
+			sibling.parent.removeChild(sibling)
+			sibling.parent.addChild(branch)
+		}
 
-		p.removeChild(sibling)
 		branch.addChild(sibling)
 		sibling.parent = branch
 
-		branch.addChild(c)
-		c.parent = branch
-	} else {
-		// add to parent
-		p.addChild(c)
-		c.parent = p
+		sibling = branch
 	}
 
-	t.recalc(p)
+	sibling.addChild(n)
+	n.parent = sibling
 }
 
 // delete branch nodes without children
@@ -112,10 +165,25 @@ func (t *SphereTree) recalc(n *Node) {
 		// leaf node, do nothing
 		return
 	}
-	if n.typ == BranchNode && len(n.children) == 0 {
+	if len(n.children) == 0 {
 		// empty branch, delete
 		if n.parent != nil {
 			n.parent.removeChild(n)
+			//t.scheduleRecalc(n.parent)
+			t.recalc(n.parent)
+		}
+		t.put(n)
+		return
+	}
+	if len(n.children) == 1 {
+		// single child, move branch up
+		if n.parent != nil {
+			n.children[0].parent = n.parent
+			n.parent.removeChild(n)
+			n.parent.addChild(n.children[0])
+		} else {
+			n.children[0].parent = nil
+			t.root = n.children[0]
 		}
 		t.put(n)
 		return
@@ -188,6 +256,24 @@ type Node struct {
 	children []*Node
 }
 
+// TODO: for debugging
+func (n *Node) String() string {
+	t := "Leaf"
+	if n.typ == BranchNode {
+		t = "Branch"
+	}
+	return fmt.Sprintf("{%s: %v: %5.2f}", t, n.center, n.radius)
+}
+
+// TODO: for debugging
+func (n *Node) StringWalk(level int) string {
+	s := strings.Repeat("  ", level) + n.String() + "\n"
+	for _, c := range n.children {
+		s += c.StringWalk(level + 1)
+	}
+	return s
+}
+
 func (n *Node) addChild(c *Node) {
 	n.children = append(n.children, c)
 }
@@ -203,37 +289,60 @@ func (n *Node) removeChild(c *Node) {
 	}
 }
 
-func (n *Node) Update(p math.Vector, r float64) {
-	if n.typ != LeafNode {
-		log.Fatalln("updating node that is not a leaf: ", n)
+func (n *Node) walk(f func(n *Node) bool) {
+	if !f(n) {
+		return
 	}
-
-	dist := n.center.Sub(p).Length()
-	if dist+r > n.radius {
-		n.center = p
-		n.radius = r
-
-		if n.parent != nil {
-			n.parent.removeChild(n)
-			n.parent = nil
-			n.tree.recalc(n.parent)
-		}
-
-		n.tree.insert(n.tree.root, n)
+	for _, c := range n.children {
+		c.walk(f)
 	}
 }
 
-func (n *Node) Delete() {
+func (n *Node) Update(p math.Vector, r float64) error {
 	if n.typ != LeafNode {
-		log.Fatalln("deleting node that is not a leaf: ", n)
+		return fmt.Errorf("updating node that is not a leaf: %v", n)
+	}
+
+	if n.parent == nil {
+		// node is root and no branch!
+		n.center = p
+		n.radius = r
+		return nil
+	}
+
+	dist := n.parent.center.Sub(p).Length()
+	if dist+r > n.parent.radius {
+		// fits no longer into parent
+		// remove from parent
+		n.parent.removeChild(n)
+		//n.tree.scheduleRecalc(n.parent)
+		n.tree.recalc(n.parent)
+		n.parent = nil
+
+		// and reinsert
+		n.center = p
+		n.radius = r
+		//n.tree.scheduleInsert(n)
+		n.tree.insert(n)
+	}
+	return nil
+}
+
+func (n *Node) Delete() error {
+	if n.typ != LeafNode {
+		return fmt.Errorf("deleting node that is not a leaf: %v", n)
 	}
 
 	if n.parent != nil {
 		n.parent.removeChild(n)
+		//n.tree.scheduleRecalc(n.parent)
 		n.tree.recalc(n.parent)
+	} else {
+		n.tree.root = nil
 	}
 
 	n.tree.put(n)
+	return nil
 }
 
 type IntersectionType int
